00fda7faec1161d1ccfbbad4dac7e05e0ab87299
Sandra Sanders <sandy-sanders@msn.com>
2021-12-12 23:16:20 -0700

    problems

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const inquirer = require('inquirer');
 const connect = require('./db/connect');
 const consoleTable = require('console.table');
 const DB = require('./db/index')
-const db = require('./db/index')
+const db = require('./db/index');
+const { addEmployee } = require('./db/index');
 
 
 
@@ -21,8 +22,8 @@ inquirer.prompt(
             message: 'What do you want to view?',
             choices: [
                 {
-                    name: 'View all departments',
-                    value: 'View_all_departments'
+                    name: 'view all departments',
+                    value: 'view_all_departments'
                 },
                 {
                     name: 'view all roles',
@@ -116,16 +117,134 @@ const viewAllRoles = () => {
         }
     });
 };
+/*WHEN I choose to view all employees
+THEN I am presented with a formatted table showing employee data, including employee ids,
+ first names, last names, job titles, departments, salaries, and managers that the employees report to*/
 
+const viewAllEmployees = () => {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title,
+    department.department-name AS department, roles.salary, 
+    CONCAT(manager.first_name'', manager.last_name) AS MANAGER FROM employee
+    LEFT JOIN roles
+    ON employee.role_id = roles.id
+    LEFT JOIN department
+    ON roles.department_id = department_id
+    LEFT JOIN employee manager
+    ON manager.id = employee.manager_id:`;
 
+    connect.query (sql, (err, res) => {
+        if (res)  {
+            const table = consoleTable.getTable(res);
+            console.log(table);
+            start();
+        }else{
+            console.log('Something went wrong', err);
+        }
+    });
+};
 
+/*WHEN I choose to add a department
+THEN I am prompted to enter the name of the department and that department is added to the database*/
+const addDepartment = () => {
+    inquirer.prompt({
+        type: 'input',
+        name: 'addDepartment',
+        message: 'Please enter department name!',
 
+    })
+    .then(res => {
+        let department = res.addDepartment;
+        DB.createDepartment(department).then(() =>
+        console.log(`add ${department}`)
+        );
+    })
+    .then(() => start());
+};
 
+/*WHEN I choose to add a role
+THEN I am prompted to enter the name, salary, and department 
+for the role and that role is added to the database*/
 
+const addRole = () => {
+    DB.findAllDepartments().then(([department]) => {
+        const departmentOptions = department.map(({ id, department_name}) => ({
+        name: department_name,
+        value: id,
+        
+    }));
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'addRole',
+        message: 'Please enter new role name!',
+      },
+      {
+          type: "input",
+          name: 'addsSalary',
+          message: 'please enter salary',
+      },
+      {
+          type: 'list',
+          name: 'Department',
+          message: 'Please enter the department for the role.',
+          choices: departmentOptions,
+      },
+    ]).then(answers => {
+        DB.createRole(answers.addRole, answers.department, answers.addSalary)
+        .then(() =>
+        console.log(`added ${answers.addRole}, added ${answers,department}, added ${answers.addSalary}`
+        )
+        )
+        .then(() => start());
+    }
+    )}
 
+};
 
+/*WHEN I choose to add an employee
+THEN I am prompted to enter the employee’s first name, last name, role, and manager, 
+and that employee is added to the database*/
 
+const addEmployee = () => {
+    DB.findAllRoles().then(([employee]) => {
+        const employeeRoleOptions = employee.map(({ id, title } => ({
+            name: title,
+            value: id,
+        }));
+        DB.findAllEmployees().then(([manager]) =>{
+            const managerOptions = manager.map(
+               ({ id, first_name, last_name}) => ({
+                   name: `${first_name} ${last_name}`,
+                   value: id,
 
+               })
+            );
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'firstName',
+                    message: 'Please enter employee first name.,'
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message: 'Please enter last name.',
+
+                },
+                {
+                    type: 'list',
+                    name: 'roles',
+                    message: ' What is the employees role?',
+                    choices: employeeRoleOptions,
+                },
+                {
+                    
+                }
+            ])
+        })
+
+    })
+}
 
 
 
@@ -134,4 +253,4 @@ const viewAllRoles = () => {
 function quit() {
     console.log("Goodbye!");
     process.exit();
-  }
+  };
